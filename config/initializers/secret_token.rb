c676fd7845e2ef435f9d88bb70c3aac42946da09
Jorge C. Avelar <jorgecavelar@gmail.com>
2011-08-01 02:41:57 -0300

    commit site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alkkmya::Application.config.secret_token = '1e2dae29e54bce3d1b1a809c10830b455a829539a9988112d91d5e4b7b71fe3082cce2d88a887e98f4c9bc29840a35070e66be9f98b7ae23177119ff6e960b64'
