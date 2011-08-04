68499ccf32aaccf3a264f6c80c4003ac2b395210
Jorge C. Avelar <jorgecavelar@gmail.com>
2011-08-04 20:06:49 -0300

    adicionando layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Alkkmya::Application.routes.draw do
   resources :contacts
 
-  get "home/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +50,7 @@ Alkkmya::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "welcome#index"
+   root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
