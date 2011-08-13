dd639ae70a63465ae26aa0e3e4e6c75898af0563
Jorge C. Avelar <jorgecavelar@gmail.com>
2011-08-13 01:14:14 -0300

    teste

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,10 @@
+class Notifier < ActionMailer::Base
+  default :from => "jorgecavelar@gmail.com"
+
+  def notification(contact)
+    mail :to = => 'jorgecavelar@gmail.com', :subject => contact.subject
+  end
+
+
+end
+
---
+++ app/models/contact_observer.rb
@@ -0,0 +1,8 @@
+class ContactObserver < ActiveRecord::Observer
+
+  def after_create(contact)
+    Notifier.notification(contact).deliver
+  end
+
+end
+
