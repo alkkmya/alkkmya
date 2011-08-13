0a3ab5e9c0a7101e59c8c55285280a718d581e63
Jorge C. Avelar <jorgecavelar@gmail.com>
2011-08-13 01:58:07 -0300

    envio de e-mail

--- app/mailers/contact_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ContactMailer < ActionMailer::Base
-  default :from => "jorgecavelar@gmail.com"
-
-  def send_mail()
-    mail(:to => 'renato.passero@alkkmya.com.br',
-         :subject => "is we")
-  end
-end
-
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   default :from => "jorgecavelar@gmail.com"
 
   def notification(contact)
-    mail :to = => 'jorgecavelar@gmail.com', :subject => contact.subject
+    mail :to => 'jorgecavelar@gmail.com', :subject => contact.subject
   end
 
 
